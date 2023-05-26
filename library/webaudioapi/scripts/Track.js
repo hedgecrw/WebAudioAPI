7e79c0dd0d7e3d2651106e510ae3e8737e0a27aa
Will Hedgecock <will.hedgecock@gmail.com>
2023-05-26 16:37:34 -0500

    Make Track a module instead of a class

--- library/webaudioapi/scripts/Track.js
+++ library/webaudioapi/scripts/Track.js
@@ -1,125 +1,123 @@
-class AsyncNoteStorage {
-   constructor(sourceNode, volumeNode) {
-      this.sourceNode = sourceNode;
-      this.volumeNode = volumeNode;
-   }
-}
+export function createTrack(audioAPI, name, trackAudioSink) {
 
-export class Track {
-   #audioSources = [];
-   #asyncAudioSources = [];
+   // Track-local variable definitions
+   let instrument = null;
+   const audioSources = [], asyncAudioSources = [];
+   const audioSink = audioAPI.audioContext.createDelay(1);
+   const volumeControlSync = audioAPI.audioContext.createGain();
+   const volumeControlAsync = audioAPI.audioContext.createGain();
+   const panningControlSync = audioAPI.audioContext.createStereoPanner();
+   const panningControlAsync = audioAPI.audioContext.createStereoPanner();
+   audioSink.connect(volumeControlSync)
+            .connect(panningControlSync)
+            .connect(volumeControlAsync)
+            .connect(panningControlAsync)
+            .connect(trackAudioSink);
+   audioSink.delayTime.setValueAtTime(0.0, 0.0);
+   panningControlSync.pan.setValueAtTime(0.0, 0.0);
+   panningControlAsync.pan.setValueAtTime(0.0, 0.0);
+   volumeControlSync.gain.setValueAtTime(1.0, 0.0);
+   volumeControlAsync.gain.setValueAtTime(1.0, 0.0);
+
+   function createAsyncNote(sourceNode, volumeNode) {
+      return { sourceNode, volumeNode }
+   }
    
-   constructor(audioAPI, trackName, audioSink) {
-      this.name = trackName;
-      this.audioAPI = audioAPI;
-      this.audioContext = audioAPI.audioContext;
-      this.audioSink = this.audioContext.createDelay(1);
-      this.volumeControlSync = this.audioContext.createGain();
-      this.volumeControlAsync = this.audioContext.createGain();
-      this.panningControlSync = this.audioContext.createStereoPanner();
-      this.panningControlAsync = this.audioContext.createStereoPanner();
-      this.audioSink.connect(this.volumeControlSync)
-                    .connect(this.panningControlSync)
-                    .connect(this.volumeControlAsync)
-                    .connect(this.panningControlAsync)
-                    .connect(audioSink);
-      this.audioSink.delayTime.setValueAtTime(0.0, 0.0);
-      this.panningControlSync.pan.setValueAtTime(0.0, 0.0);
-      this.panningControlAsync.pan.setValueAtTime(0.0, 0.0);
-      this.volumeControlSync.gain.setValueAtTime(1.0, 0.0);
-      this.volumeControlAsync.gain.setValueAtTime(1.0, 0.0);
-      this.instrument = null;
-   }
-
-   get instrumentName() {
-      return (this.instrument == null) ? 'None' : this.instrument.name;
-   }
-
-   #sourceEnded(source, sourceVolume) {
+   function sourceEnded(source, sourceVolume) {
       if (sourceVolume == null)
          sourceVolume = source;
       sourceVolume.disconnect();
-      this.#audioSources.splice(this.#audioSources.indexOf(source), 1);
+      audioSources.splice(audioSources.indexOf(source), 1);
    }
 
-   changeInstrument(instrument) {
-      this.instrument = instrument;
+   function instrumentName() {
+      return (instrument == null) ? 'None' : instrument.name;
    }
 
-   updateVolume(percent, updateTime) {
+   function changeInstrument(newInstrument) {
+      instrument = newInstrument;
+   }
+
+   function updateVolume(percent, updateTime) {
       if (updateTime == null)
-         this.volumeControlAsync.gain.setTargetAtTime(percent, this.audioContext.currentTime, 0.01);
+         volumeControlAsync.gain.setTargetAtTime(percent, audioAPI.audioContext.currentTime, 0.01);
       else
-         this.volumeControlSync.gain.setTargetAtTime(percent, updateTime, 0.01);
+         volumeControlSync.gain.setTargetAtTime(percent, updateTime, 0.01);
    }
 
-   updatePanning(percent, updateTime) {
+   function updatePanning(percent, updateTime) {
       if (updateTime == null)
-         this.panningControlAsync.pan.setTargetAtTime((2 * percent) - 1, this.audioContext.currentTime, 0.01);
+         panningControlAsync.pan.setTargetAtTime((2 * percent) - 1, audioAPI.audioContext.currentTime, 0.01);
       else
-         this.panningControlSync.pan.setTargetAtTime((2 * percent) - 1, updateTime, 0.01);
+         panningControlSync.pan.setTargetAtTime((2 * percent) - 1, updateTime, 0.01);
    }
 
-   playNoteAsync(note) {
-      const noteSource = this.instrument.getNote(this.audioContext, note); // TODO: Method to getNoteContinuous so it loops
-      const noteVolume = this.audioContext.createGain();
-      noteSource.connect(noteVolume).connect(this.audioSink);
-      noteVolume.gain.setValueAtTime(1.0, 0.0);
-      const noteStorage = new AsyncNoteStorage(noteSource, noteVolume);
-      noteSource.onended = this.stopNoteAsync.bind(this, noteStorage);
-      this.#asyncAudioSources.push(noteStorage);
-      noteSource.start(this.audioContext.currentTime);
-      return noteStorage;
-   }
-
-   stopNoteAsync(noteObject) {
-      this.#asyncAudioSources.splice(this.#asyncAudioSources.indexOf(noteObject), 1);
-      noteObject.volumeNode.gain.setTargetAtTime(0.0, this.audioContext.currentTime, 0.1);
+   function stopNoteAsync(noteObject) {
+      asyncAudioSources.splice(asyncAudioSources.indexOf(noteObject), 1);
+      noteObject.volumeNode.gain.setTargetAtTime(0.0, audioAPI.audioContext.currentTime, 0.1);
       setTimeout(function() {
          noteObject.sourceNode.stop();
          noteObject.volumeNode.disconnect();
       }, 200);
    }
 
-   playNote(note, startTime, duration) {
-      const durationSeconds = 60.0 / ((duration / this.audioAPI.beatBase) * this.audioAPI.beatsPerMinute);
-      const noteSource = this.instrument.getNote(this.audioContext, note);
-      const noteVolume = this.audioContext.createGain();
-      noteSource.connect(noteVolume).connect(this.audioSink);
-      noteVolume.gain.setValueAtTime(1.0, 0.0);
-      noteVolume.gain.setTargetAtTime(0.0, startTime + durationSeconds - 0.1, 0.1);
-      noteSource.onended = this.#sourceEnded.bind(this, noteSource, noteVolume);
-      this.#audioSources.push(noteSource);
-      noteSource.start(startTime);
-      noteSource.stop(startTime + durationSeconds);
-      return durationSeconds;
+   function playNoteAsync(note) {
+      if (instrument) {
+         const noteSource = instrument.getNote(audioAPI.audioContext, note); // TODO: Method to getNoteContinuous so it loops
+         const noteVolume = audioAPI.audioContext.createGain();
+         noteSource.connect(noteVolume).connect(audioSink);
+         noteVolume.gain.setValueAtTime(1.0, 0.0);
+         const noteStorage = createAsyncNote(noteSource, noteVolume);
+         noteSource.onended = stopNoteAsync.bind(this, noteStorage); // TODO: Don't need this if continuous instrument
+         asyncAudioSources.push(noteStorage);
+         noteSource.start(audioAPI.audioContext.currentTime);
+         return noteStorage;
+      }
+      return null;
    }
 
-   async playClip(buffer, startTime) {
-      const audioBuffer = await this.audioContext.decodeAudioData(buffer);
-      const clipSource = this.audioContext.createBufferSource();
+   function playNote(note, startTime, duration) {
+      if (instrument) {
+         const durationSeconds = 60.0 / ((duration / audioAPI.beatBase) * audioAPI.beatsPerMinute);
+         const noteSource = instrument.getNote(audioAPI.audioContext, note);
+         const noteVolume = audioAPI.audioContext.createGain();
+         noteSource.connect(noteVolume).connect(audioSink);
+         noteVolume.gain.setValueAtTime(1.0, 0.0);
+         noteVolume.gain.setTargetAtTime(0.0, startTime + durationSeconds - 0.1, 0.1);
+         noteSource.onended = sourceEnded.bind(this, noteSource, noteVolume);
+         audioSources.push(noteSource);
+         noteSource.start(startTime);
+         noteSource.stop(startTime + durationSeconds);
+         return durationSeconds;
+      }
+      return 0;
+   }
+
+   async function playClip(buffer, startTime) {
+      const audioBuffer = await audioAPI.audioContext.decodeAudioData(buffer);
+      const clipSource = audioAPI.audioContext.createBufferSource();
       clipSource.buffer = audioBuffer;
-      clipSource.connect(this.audioSink);
-      clipSource.onended = this.#sourceEnded.bind(this, clipSource, null);
-      this.#audioSources.push(clipSource);
+      clipSource.connect(audioSink);
+      clipSource.onended = sourceEnded.bind(this, clipSource, null);
+      audioSources.push(clipSource);
       clipSource.start(startTime);
       return audioBuffer.duration;
    }
 
-   async playFile(file, startTime) {
+   async function playFile(file, startTime) {
       const response = await fetch(file);
       const arrayBuffer = await response.arrayBuffer();
-      return await this.playClip(arrayBuffer, startTime);
+      return await playClip(arrayBuffer, startTime);
    }
 
-   delete() {
-      for (const source of this.#audioSources)
+   function deleteTrack() {
+      for (const source of audioSources)
          source.stop();
-      for (const source of this.#asyncAudioSources)
+      for (const source of asyncAudioSources)
          source.sourceNode.stop();
-      if (this.panningControlAsync != null)
-         this.panningControlAsync.disconnect();
-      this.instrument = this.panningControlSync = this.panningControlAsync = null;
-      this.audioSink = this.volumeControlSync = this.volumeControlAsync = null;
+      panningControlAsync.disconnect();
    }
+
+   return { name, instrumentName, changeInstrument, updateVolume, updatePanning,
+      playNote, playFile, playClip, playNoteAsync, stopNoteAsync, deleteTrack };
 }

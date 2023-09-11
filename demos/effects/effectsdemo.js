1e15d9156255e7210df85bb3a1ee48e4632cc880
Will Hedgecock <will.hedgecock@gmail.com>
2023-09-10 21:31:46 -0500

    Implement Equalizer effect

--- demos/effects/effectsdemo.js
+++ demos/effects/effectsdemo.js
@@ -6,54 +6,128 @@ function changeParameter(event) {
    }
 }
 
-function changeEffect(effectType) {
-   const currentEffectSelection = document.getElementById('effectSelector');
+function changeFrequencyParameter(event) {
+   const effectOptions = { 'frequencyBandUpperCutoffs': [], 'frequencyBandGains': [] };
+   for (const target of document.querySelectorAll('input[type=range]')) {
+      if (target.name.includes('band'))
+         effectOptions['frequencyBandUpperCutoffs'].push(Number(target.value));
+      else
+         effectOptions['frequencyBandGains'].push(Number(target.value));
+      target.title = target.value;
+   }
+   window.audioAPI.updateTrackEffect('defaultTrack', window.currentEffect, effectOptions);
+}
+
+function changeNumFrequencyBands(event) {
    const effectOptionsContainer = document.getElementById('effectOptions');
-   const effectText = currentEffectSelection.options[currentEffectSelection.options.selectedIndex].innerText;
-   while (effectOptionsContainer.firstChild)
-      effectOptionsContainer.removeChild(effectOptionsContainer.firstChild);
-   for (const effectOption of window.audioAPI.getAvailableEffectParameters(effectType)) {
+   if ((1 + (2 * Number(event.target.value))) != effectOptionsContainer.children.length) {
+      while (effectOptionsContainer.firstChild)
+         effectOptionsContainer.removeChild(effectOptionsContainer.firstChild);
       const effectOptionContainer = document.createElement('tr');
       const labelContainer = document.createElement('td'), valueContainer = document.createElement('td');
       effectOptionContainer.appendChild(labelContainer);
       effectOptionContainer.appendChild(valueContainer);
       effectOptionsContainer.appendChild(effectOptionContainer);
       const optionLabel = document.createElement('label');
-      optionLabel.htmlFor = effectOption.name;
-      optionLabel.innerText = effectOption.name + ':';
+      optionLabel.htmlFor = 'numFrequencyBands';
+      optionLabel.innerText = 'numFrequencyBands:';
       labelContainer.appendChild(optionLabel);
-      if (effectOption.type == 'number') {
+      const preSpan = document.createElement('span'), postSpan = document.createElement('span');
+      preSpan.innerHTML = '&nbsp;'; postSpan.innerHTML = '&nbsp;';
+      const optionSelection = document.createElement('select');
+      optionSelection.id = 'numFrequencyBands';
+      optionSelection.name = 'numFrequencyBands';
+      for (let i = 2; i < 10; ++i)
+         optionSelection.add(new Option(i, i));
+      optionSelection.value = Number(event.target.value);
+      optionSelection.addEventListener('change', changeNumFrequencyBands);
+      valueContainer.appendChild(preSpan);
+      valueContainer.appendChild(optionSelection);
+      valueContainer.appendChild(postSpan);
+      for (let i = 0; i < 2 * Number(event.target.value); ++i) {
+         const integerI = parseInt(i/2);
+         const effectOptionContainer = document.createElement('tr');
+         const labelContainer = document.createElement('td'), valueContainer = document.createElement('td');
+         effectOptionContainer.appendChild(labelContainer);
+         effectOptionContainer.appendChild(valueContainer);
+         effectOptionsContainer.appendChild(effectOptionContainer);
+         const optionLabel = document.createElement('label');
+         optionLabel.htmlFor = ((i % 2) == 0) ? ('band' + integerI) : ('gain' + integerI);
+         optionLabel.innerText = 'frequencyBand' + (((i % 2) == 0) ? 'UpperCutoff #' : 'Gain #') + (integerI+1) + ':';
+         labelContainer.appendChild(optionLabel);
          const optionSelection = document.createElement('input');
-         optionSelection.title = effectOption.defaultValue;
-         optionSelection.id = effectOption.name;
-         optionSelection.name = effectOption.name;
+         optionSelection.title = ((i % 2) == 0) ? ((integerI+1) * (22050 / Number(event.target.value))) : 0;
+         optionSelection.id = ((i % 2) == 0) ? ('band' + integerI) : ('gain' + integerI);
+         optionSelection.name = ((i % 2) == 0) ? ('band' + integerI) : ('gain' + integerI);
          optionSelection.type = 'range';
-         optionSelection.min = effectOption.validValues[0];
-         optionSelection.max = effectOption.validValues[1];
+         optionSelection.min = ((i % 2) == 0) ? 1000 : -40;
+         optionSelection.max = ((i % 2) == 0) ? 22050 : 40;
          optionSelection.step = 0.001;
-         optionSelection.value = effectOption.defaultValue;
-         optionSelection.addEventListener('input', changeParameter);
+         optionSelection.value = ((i % 2) == 0) ? ((integerI+1) * (22050 / Number(event.target.value))) : 0;
+         optionSelection.addEventListener('input', changeFrequencyParameter);
          const minValueLabel = document.createElement('span'), maxValueLabel = document.createElement('span');
          minValueLabel.className = 'right';
-         minValueLabel.innerHTML = effectOption.validValues[0] + '&nbsp;';
-         maxValueLabel.innerHTML = '&nbsp;' + effectOption.validValues[1];
+         minValueLabel.innerHTML = (((i % 2) == 0) ? '0' : '-40') + '&nbsp;';
+         maxValueLabel.innerHTML = '&nbsp;' + (((i % 2) == 0) ? '22050' : '40');
          valueContainer.appendChild(minValueLabel);
          valueContainer.appendChild(optionSelection);
          valueContainer.appendChild(maxValueLabel);
       }
-      else {
-         const preSpan = document.createElement('span'), postSpan = document.createElement('span');
-         preSpan.innerHTML = '&nbsp;'; postSpan.innerHTML = '&nbsp;';
-         const optionSelection = document.createElement('select');
-         optionSelection.id = effectOption.name;
-         optionSelection.name = effectOption.name;
-         for (const value of effectOption.validValues)
-            optionSelection.add(new Option(value, value));
-         optionSelection.value = effectOption.defaultValue;
-         optionSelection.addEventListener('change', changeParameter);
-         valueContainer.appendChild(preSpan);
-         valueContainer.appendChild(optionSelection);
-         valueContainer.appendChild(postSpan);
+   }
+}
+
+function changeEffect(effectType) {
+   const currentEffectSelection = document.getElementById('effectSelector');
+   const effectOptionsContainer = document.getElementById('effectOptions');
+   const effectText = currentEffectSelection.options[currentEffectSelection.options.selectedIndex].innerText;
+   while (effectOptionsContainer.firstChild)
+      effectOptionsContainer.removeChild(effectOptionsContainer.firstChild);
+   if (effectText == 'Equalization')
+      changeNumFrequencyBands({target: {value: 2}});
+   else {
+      for (const effectOption of window.audioAPI.getAvailableEffectParameters(effectType)) {
+         const effectOptionContainer = document.createElement('tr');
+         const labelContainer = document.createElement('td'), valueContainer = document.createElement('td');
+         effectOptionContainer.appendChild(labelContainer);
+         effectOptionContainer.appendChild(valueContainer);
+         effectOptionsContainer.appendChild(effectOptionContainer);
+         const optionLabel = document.createElement('label');
+         optionLabel.htmlFor = effectOption.name;
+         optionLabel.innerText = effectOption.name + ':';
+         labelContainer.appendChild(optionLabel);
+         if (effectOption.type == 'number') {
+            const optionSelection = document.createElement('input');
+            optionSelection.title = effectOption.defaultValue;
+            optionSelection.id = effectOption.name;
+            optionSelection.name = effectOption.name;
+            optionSelection.type = 'range';
+            optionSelection.min = effectOption.validValues[0];
+            optionSelection.max = effectOption.validValues[1];
+            optionSelection.step = 0.001;
+            optionSelection.value = effectOption.defaultValue;
+            optionSelection.addEventListener('input', changeParameter);
+            const minValueLabel = document.createElement('span'), maxValueLabel = document.createElement('span');
+            minValueLabel.className = 'right';
+            minValueLabel.innerHTML = effectOption.validValues[0] + '&nbsp;';
+            maxValueLabel.innerHTML = '&nbsp;' + effectOption.validValues[1];
+            valueContainer.appendChild(minValueLabel);
+            valueContainer.appendChild(optionSelection);
+            valueContainer.appendChild(maxValueLabel);
+         }
+         else if (effectOption.type == 'string') {
+            const preSpan = document.createElement('span'), postSpan = document.createElement('span');
+            preSpan.innerHTML = '&nbsp;'; postSpan.innerHTML = '&nbsp;';
+            const optionSelection = document.createElement('select');
+            optionSelection.id = effectOption.name;
+            optionSelection.name = effectOption.name;
+            for (const value of effectOption.validValues)
+               optionSelection.add(new Option(value, value));
+            optionSelection.value = effectOption.defaultValue;
+            optionSelection.addEventListener('change', changeParameter);
+            valueContainer.appendChild(preSpan);
+            valueContainer.appendChild(optionSelection);
+            valueContainer.appendChild(postSpan);
+         }
       }
    }
    if (effectText == 'Doppler') {
--- library/webaudioapi/effects/Equalization.mjs
+++ library/webaudioapi/effects/Equalization.mjs
@@ -12,11 +12,25 @@ import { EffectBase } from './EffectBase.mjs';
  */
 export class Equalization extends EffectBase {
 
+   // Effect-specific private variables
+   /** @type {BiquadFilterNode[]} */
+   #equalizerNodes;
+
+   // Parameter limits
+   static minGain = -40;
+   static maxGain = 40;
+   static minCutoff = 1000;
+   static maxCutoff = 22050;
+
    /**
     * Constructs a new {@link Equalization} effect object.
     */
    constructor(audioContext) {
       super(audioContext);
+      this.#equalizerNodes = [
+         new BiquadFilterNode(audioContext, { type: 'lowshelf', frequency: audioContext.sampleRate / 4, gain: 0 }),
+         new BiquadFilterNode(audioContext, { type: 'highshelf', frequency: audioContext.sampleRate / 4, gain: 0 })
+      ];
    }
 
    /**
@@ -27,11 +41,14 @@ export class Equalization extends EffectBase {
     * @see {@link EffectParameter}
     */
    static getParameters() {
-      return [];
+      return [
+         { name: 'frequencyBandUpperCutoffs', type: 'Array<number>', validValues: [Equalization.minCutoff, Equalization.maxCutoff], defaultValue: 0 },
+         { name: 'frequencyBandGains', type: 'Array<number>', validValues: [Equalization.minGain, Equalization.maxGain], defaultValue: 0 },
+      ];
    }
 
    async load() {
-      return;
+      this.#equalizerNodes[0].connect(this.#equalizerNodes[1]);
    }
 
    /**
@@ -41,25 +58,73 @@ export class Equalization extends EffectBase {
     * Note that the `updateTime` parameter can be omitted to immediately cause the requested
     * changes to take effect.
     * 
-    * @param {number[]} frequencyBandUpperCutoffs - Upper frequency cutoffs for each band in the equalizer
-    * @param {number[]} frequencyBandVolumes - Volumes for each frequency band in the equalizer
+    * @param {number[]} frequencyBandUpperCutoffs - Upper frequency cutoffs in Hz for each band in the equalizer between [0, 22050]
+    * @param {number[]} frequencyBandGains - Gains in dB for each frequency band in the equalizer between [-40, 40]
     * @param {number} [updateTime] - Global API time at which to update the effect
     * @param {number} [timeConstant] - Time constant defining an exponential approach to the target
     * @returns {Promise<boolean>} Whether the effect update was successfully applied
     */
-   async update({frequencyBandUpperCutoffs, frequencyBandVolumes}, updateTime, timeConstant) {
-      if ((frequencyBandUpperCutoffs == null) && (frequencyBandVolumes == null))
-         throw new WebAudioApiErrors.WebAudioValueError('Cannot update the Equalization effect without at least one of the following parameters: "frequencyBandUpperCutoffs, frequencyBandVolumes"');
+   async update({ frequencyBandUpperCutoffs, frequencyBandGains }, updateTime, timeConstant) {
+      if ((frequencyBandUpperCutoffs == null) || (frequencyBandGains == null))
+         throw new WebAudioApiErrors.WebAudioValueError('Cannot update the Equalization effect without both of the following parameters: "frequencyBandUpperCutoffs, frequencyBandGains"');
+      if (frequencyBandUpperCutoffs.length != frequencyBandGains.length)
+         throw new WebAudioApiErrors.WebAudioValueError('Frequency cutoff array and frequency gain array must have the same size');
+      for (const cutoff of frequencyBandUpperCutoffs) {
+         if (cutoff < Equalization.minCutoff)
+            throw new WebAudioApiErrors.WebAudioValueError(`Frequency upper cutoff value cannot be less than ${Equalization.minCutoff}`);
+         else if (cutoff > Equalization.maxCutoff)
+            throw new WebAudioApiErrors.WebAudioValueError(`Frequency upper cutoff value cannot be greater than ${Equalization.maxCutoff}`);
+      }
+      for (const gain of frequencyBandGains) {
+         if (gain < Equalization.minGain)
+            throw new WebAudioApiErrors.WebAudioValueError(`Gain value cannot be less than ${Equalization.minGain}`);
+         else if (gain > Equalization.maxGain)
+            throw new WebAudioApiErrors.WebAudioValueError(`Gain value cannot be greater than ${Equalization.maxGain}`);
+      }
+      for (let i = 1; i < frequencyBandUpperCutoffs.length; ++i) {
+         if (frequencyBandUpperCutoffs[i] <= frequencyBandUpperCutoffs[i-1])
+            throw new WebAudioApiErrors.WebAudioValueError('Frequency band upper cutoffs must be monotonically increasing within the array');
+      }
       const timeToUpdate = (updateTime == null) ? this.audioContext.currentTime : updateTime;
       const timeConstantTarget = (timeConstant == null) ? 0.0 : timeConstant;
-      return false;
+
+      // Ensure the correct number of equalization bands are present
+      if (frequencyBandUpperCutoffs.length < this.#equalizerNodes.length) {
+         this.#equalizerNodes[0].connect(this.#equalizerNodes[this.#equalizerNodes.length - frequencyBandUpperCutoffs.length + 1]);
+         for (const removedNode of this.#equalizerNodes.splice(1, this.#equalizerNodes.length - frequencyBandUpperCutoffs.length))
+            removedNode.disconnect();
+      }
+      else if (frequencyBandUpperCutoffs.length > this.#equalizerNodes.length) {
+         const lastNode = this.#equalizerNodes.splice(-1, 1)[0];
+         this.#equalizerNodes[this.#equalizerNodes.length - 1].disconnect();
+         while ((this.#equalizerNodes.length + 1) < frequencyBandUpperCutoffs.length) {
+            const newNode = new BiquadFilterNode(this.audioContext, { type: 'peaking' });
+            this.#equalizerNodes[this.#equalizerNodes.length - 1].connect(newNode);
+            this.#equalizerNodes.push(newNode);
+         }
+         this.#equalizerNodes[this.#equalizerNodes.length - 1].connect(lastNode);
+         this.#equalizerNodes.push(lastNode);
+      }
+
+      // Update the parameters for each equalization band
+      this.#equalizerNodes[0].frequency.setTargetAtTime(frequencyBandUpperCutoffs[0], timeToUpdate, timeConstantTarget);
+      this.#equalizerNodes[0].gain.setTargetAtTime(frequencyBandGains[0], timeToUpdate, timeConstantTarget);
+      this.#equalizerNodes[this.#equalizerNodes.length-1].frequency.setTargetAtTime(frequencyBandUpperCutoffs[frequencyBandUpperCutoffs.length-2], timeToUpdate, timeConstantTarget);
+      this.#equalizerNodes[this.#equalizerNodes.length-1].gain.setTargetAtTime(frequencyBandGains[frequencyBandGains.length-1], timeToUpdate, timeConstantTarget);
+      for (let i = 1; i < this.#equalizerNodes.length - 1; ++i) {
+         const centerFrequency = 0.5 * (frequencyBandUpperCutoffs[i] + frequencyBandUpperCutoffs[i-1]);
+         this.#equalizerNodes[i].frequency.setTargetAtTime(centerFrequency, timeToUpdate, timeConstantTarget);
+         this.#equalizerNodes[i].Q.setTargetAtTime(centerFrequency / (frequencyBandUpperCutoffs[i] - frequencyBandUpperCutoffs[i-1]), timeToUpdate, timeConstantTarget);
+         this.#equalizerNodes[i].gain.setTargetAtTime(frequencyBandGains[i], timeToUpdate, timeConstantTarget);
+      }
+      return true;
    }
 
    getInputNode() {
-      return;
+      return this.#equalizerNodes[0];
    }
 
    getOutputNode() {
-      return;
+      return this.#equalizerNodes[this.#equalizerNodes.length - 1];
    }
 }
